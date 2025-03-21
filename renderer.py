c70275e51b8725edae0f2d333874dc41004335c8
lake song <mania2785@gmail.com>
2025-03-22 02:15:49 +0900

    Add sub mesh save function (#3)

--- renderer.py
+++ renderer.py
@@ -1,20 +1,22 @@
 import pyvista as pv
+from pyvista.core.pointset import PolyData
 
 class MeshRenderer:
-    def __init__(self, mesh):
+    def __init__(self, mesh: list[PolyData]):
         self.mesh = mesh
-        self.actor_group_mapping = {}
         self.plotter = pv.Plotter(window_size=[1920, 1080])
         self._setup_plotter()
         
     def _setup_plotter(self):
         self.plotter.add_text("Left-click on a face to highlight its group", 
                               font_size=12, color="white")
-        self.plotter.add_mesh(self.mesh, color="lightgray", pickable=True)
+        
+        for i, mesh in enumerate(self.mesh):
+            self.plotter.add_mesh(mesh, color='lightgray', label=f"Face {i}")
 
         self.plotter.enable_anti_aliasing('fxaa')
 
-        self.plotter.enable_element_picking(callback=self.pick_callback, mode='face', left_clicking=True, show_message=False)
+        self.plotter.enable_element_picking(callback=self.pick_callback, mode='mesh', left_clicking=True, show_message=False)
         self.plotter.reset_camera()
 
     def pick_callback(self, picked_mesh):
--- utils.py
+++ utils.py
@@ -3,7 +3,36 @@ import pyvista as pv
 
 class Utils:
     def __init__(self, filepath):
-        step_file = cq.importers.importStep(filepath)
-        cq.exporters.export(step_file, 'converted.stl', exportType='STL')
+        self.mesh = []
 
-        self.mesh = pv.read('converted.stl')
+        try:
+            step_file = cq.importers.importStep(filepath)
+            solids = step_file.solids()
+
+            if not isinstance(solids, list):
+                solids = [solids]
+        except Exception as e:
+            print(f"STEP file error: {e}")
+            exit()
+
+        obj_filepaths = []
+
+        for s_idx, solid in enumerate(solids):
+            for f_idx, face in enumerate(solid.faces()):
+                mesh_data = face.tessellate(tolerance=0.1)
+                if mesh_data:
+                    vertices, triangles = mesh_data
+                    obj_filepath = f"solid_{s_idx}_face_{f_idx}.obj"
+                    self.export_obj(vertices, triangles, obj_filepath)
+                    print(f"Solid {s_idx} Face {f_idx} OBJ Saved: {obj_filepath}")
+                    obj_filepaths.append(obj_filepath)
+
+        for obj_filepath in obj_filepaths:
+            self.mesh.append(pv.read(obj_filepath))
+
+    def export_obj(self, vertices, triangles, filename):
+        with open(filename, 'w') as f:
+            for vertex in vertices:
+                f.write("v {} {} {}\n".format(vertex.x, vertex.y, vertex.z))
+            for triangle in triangles:
+                f.write("f {} {} {}\n".format(triangle[0] + 1, triangle[1] + 1, triangle[2] + 1))
